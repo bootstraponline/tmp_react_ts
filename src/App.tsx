6569042dbd79b988cb2550b1cea741d8e9d38095
bootstraponline <matt@bootstraponline.com>
2018-03-01 16:00:55 -0500

    Refactor data into data.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,63 +14,27 @@ import {
 import { initializeIcons } from 'office-ui-fabric-react/lib/Icons';
 initializeIcons();
 
- /* tslint:disable:no-debugger */
+import { IItem, dataItems, dataGroups } from './data';
 
-interface Item {
-  /**
-   * Unique identifier for the item.
-   */
-  key: string;
-  /**
-   * Display name for the item, rendered on the header.
-   */
-  name: string;
-}
+/* tslint:disable:no-debugger */
 
-let _items: Item[];
+let _items: IItem[];
 let _groups: IGroup[];
 
 class App extends React.Component {
   private _selection: Selection;
   constructor(props: {}) {
     super(props);
-    _items = [
-      {
-        key: 'item-0',
-        name: 'android.support.test.espresso.NoMatchingViewException: No views in hierarchy found matching: (with text: is "Everyone" and has sibling: with id: com.instructure.teacher:id/assigneeSubtitleView)'
-      },
-      {
-        key: 'item-1',
-        name: 'java.lang.RuntimeException: Waited for the root of the view hierarchy to have window focus and not request layout for 10 seconds. If you specified a non default root matcher, it may be picking a root that never takes focus.'
-      }
-    ];
+
+    _items = dataItems;
+    _groups = dataGroups;
+
     /* tslint:disable:max-line-length */
     // selection is required.
     // toggleRangeSelected errors on group click if selection is null
     this._selection = new Selection();
-
-    const group0: IGroup = {
-      key: 'group-0',
-      name: 'AssigneeListPageTest#displaysStudentItems',
-      startIndex: 0,
-      count: 1,
-    };
-
-    const group1: IGroup = {
-      key: 'group-1',
-      name: 'SpeedGraderCommentsPageTest#displaysAuthorName',
-      startIndex: 1,
-      count: 1,
-    };
-
-    _groups = [group0, group1];
-
     this._onRenderCell = this._onRenderCell.bind(this);
     this._onRenderHeader = this._onRenderHeader.bind(this);
-  }
-
-  public testAlert(): void {
-    alert('hello');
   }
 
   public render(): JSX.Element {
@@ -103,7 +67,7 @@ class App extends React.Component {
     return <GroupHeader { ...props } />;
   }
 
-  private _onRenderCell(nestingDepth: number, item: Item, itemIndex: number): JSX.Element {
+  private _onRenderCell(nestingDepth: number, item: IItem, itemIndex: number): JSX.Element {
     return (
       <div data-selection-index={itemIndex}>
         <span className='GroupedList-name'>
